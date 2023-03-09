3de2f682e96f8afbbc43d0f1c4a7268ff8ecfe03
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-09 20:34:02 +0530

    improved schema

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,8 +4,9 @@ const getAllTasks = (req, res) => {
   res.send("All Tasks.");
 };
 
-const createTasks = (req, res) => {
-  res.json(req.body);
+const createTasks = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json(task);
 };
 
 const getTask = (req, res) => {
--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  Completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Must Provide a Name!"],
+    trim: true,
+    maxlength: [20, "Name Can't be More than 20 Characters!"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
