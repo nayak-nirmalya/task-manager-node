d32b3a362795d6c4cdd773963ec156dd749df1ef
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-09 20:48:29 +0530

    added logic for getTask/s, createTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,38 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("All Tasks.");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const createTasks = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json(task);
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ message: `No Task With ID: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const updateTask = (req, res) => {
