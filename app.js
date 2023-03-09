0a7b31e26dad303aefe13277207e8b5a752e83d2
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-09 15:15:55 +0530

    added routes

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+const PORT = 3000;
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manger App.");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(PORT, console.log(`Server is Listening on Port: ${PORT}...`));
